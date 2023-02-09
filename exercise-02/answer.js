ab7b041d30c111dff6f4585c94a5ff87f3cf517d
Baipor <baipor2545@hotmail.com>
2023-02-10 00:52:19 +0700

    Update answer.js

--- exercise-02/answer.js
+++ exercise-02/answer.js
@@ -7,16 +7,31 @@ function display(msg) {
 
 function showConfirm(callback) {
     // You code here
-    alert('confirm')
+    if(confirm("Okay?")){
+        //call function
+        callback("ยืนยันจร้า")
+    }
+    else{
+        //call no function
+        callback("ไม่ดีกว่า")
+    }
 }
 
 // ข้อ 2.2
 function start() {
     // hint: ส่ง callback function เข้าไปเป็น argument ของ setTimeout()
+    console.log("Program started")
+    setTimeout(()=>{
+        console.log("Hello World")
+        setTimeout(()=>{
+            console.log("Program ended")
+        },3000)
+    },2000)
 }
 
 // ข้อ 2.3
 function stopTime() {
-
+    let secs = document.getElementById("Time").value
+    document.getElementById('setSecond').innerHTML = secs
 }
 
